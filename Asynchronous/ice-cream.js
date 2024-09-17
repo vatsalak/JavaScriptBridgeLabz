e888e4db7a490b1bdb8f257b2260067d31d3ac79
Vatsala Kaashyap <vatsalakaashyap@gmail.com>
2024-09-17 15:55:34 +0530

    Done with asynchronous

---
+++ Asynchronous/ice-cream.js
@@ -0,0 +1,14 @@
+//Synchronous
+console.log(" I ");
+console.log(" eat ");
+console.log( " ice-cream ")
+console.log(" with a ");
+console.log(" spoon ");
+
+//Asynchronous
+console.log(" I ");
+console.log(" eat ");
+setTimeout(()=>{console.log(" ice cream ");  
+},4000);
+console.log(" with a ");
+console.log(" spoon ");
