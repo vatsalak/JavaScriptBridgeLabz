60a212c0463ecbeaf34e8dffec70072b5c9f7908
Vatsala Kaashyap <vatsalakaashyap@gmail.com>
2024-09-21 14:35:17 +0530

    Done with ES7

---
+++ ES7/ExponentiationOperator.js
@@ -0,0 +1,5 @@
+let power=(2**10);
+console.log(power);
+
+let pow=Math.pow(2,10);
+console.log(pow);
---
+++ ES7/array_prototype_include.js
@@ -0,0 +1,6 @@
+const arr=[1,2,3,4,5];
+if (arr.includes(3))
+{
+    console.log("The element 3 is in the index"+arr.indexOf(3));
+    ;
+}
